2ca15395b32d0f67f0f4430a496d9572dfef62a9
Yuto Watanabe <yuto.w51942@gmail.com>
2020-12-28 21:32:29 +0900

    update url

--- unchi-maker/pages/index.tsx
+++ unchi-maker/pages/index.tsx
@@ -4,7 +4,7 @@ import Unchi from '../components/Unchi'
 function HomePage() {
   return (
     <Page>
-      <Unchi url={'https://example.com'} />
+      <Unchi url={'https://unchi-maker-site-m3rjvc8fl.vercel.app/'} />
     </Page>
   )
 }
